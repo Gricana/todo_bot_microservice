96b22ef4fb406c53ca36314884f2fc8c57407784
Gricana <alex2904.goryachev@yandex.com>
2025-03-11 19:02:46 +0300

    feat(infra/api): add API entrypoint and process supervision

---
+++ todo_api/infra/api/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+./infra/db/wait-for-it.sh db:5432 -- echo "Database is up"
+
+python manage.py makemigrations tasks users --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Create admin user
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.getenv("ADMIN_USERNAME", "admin")
+email = os.getenv("ADMIN_EMAIL", "admin@example.com")
+password = os.getenv("ADMIN_PASSWORD", "admin123")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created")
+else:
+    print("Superuser already exists")
+EOF
+
+supervisord -c /etc/supervisord.conf
