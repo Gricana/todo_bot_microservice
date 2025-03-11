cc86ffc0f7d8409e4acfc2c1bc82f054ce030f1a
Gricana <alex2904.goryachev@yandex.com>
2025-03-11 22:45:40 +0300

    fix(infra/db): changed the configuration pg_hba.conf

--- comments_api/infra/db/init.sh
+++ comments_api/infra/db/init.sh
@@ -4,3 +4,7 @@
 PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE \"$DB_NAME\";" || true
 PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d postgres -c "CREATE USER \"$DB_USER\" WITH PASSWORD '$DB_PASSWORD';" || true
 PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$DB_NAME\" TO \"$DB_USER\";" || true
+
+echo "Configuring PostgreSQL..."
+echo "host all all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
+pg_ctl reload
