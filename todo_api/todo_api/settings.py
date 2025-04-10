0c5d4d1061da581176db1b149bbdd4de82b51bab
Gricana <alex2904.goryachev@yandex.com>
2025-04-10 20:01:32 +0300

    feat(todo_api): added settings for the correct work with the frontend server

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "django_celery_beat",
     "drf_yasg",
+    "corsheaders",
     # My apps
     "tasks",
     "users",
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -221,3 +223,6 @@ LOGGING = {
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
